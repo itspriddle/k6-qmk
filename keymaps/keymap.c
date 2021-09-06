420ad4e9fcd5a96de70c64fe3b9cf0be9306a28a
Joshua Priddle <jpriddle@me.com>
2021-09-06 02:06:16 -0400

    Use _K6_WINDOWS as a build option for Windows keys

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -16,29 +16,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
+// The Mac/Windows switch doesn't seem to trigger `dip_switch_update_user` or
+// `dip_switch_update_kb`. I only use a Macso I'm defaulting to that. If you
+// want Windows, compile with `make keychron/k6:ansi-josh _K6_WINDOWS=yes` or
+// uncomment the line below
+/* #define _K6_WINDOWS */
+
+// Each layer gets a name for readability, which is then used in the keymap
+// matrix below. The underscores don't mean anything - you can have a layer
+// called STUFF or any other name. Layer names don't all need to be of the
+// same length, obviously, and you can also skip them entirely and just use
+// numbers.
 enum layer_names {
-    _MAC_BASE,
-    _MAC_FN1,
-    _MAC_FN2,
-    _WIN_BASE,
-    _WIN_FN1,
-    _WIN_FN2,
+    _BASE = 0,
+    _FN1,
+    _FN2,
 };
 
 // Custom keykodes
 enum my_keycodes {
-  MAC_EXPOSE = SAFE_RANGE,
-  SET_MAC,
-  SET_WIN
+  MAC_EXPOSE = SAFE_RANGE
 };
 
 // https://beta.docs.qmk.fm/using-qmk/simple-keycodes/keycodes
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+#ifndef _K6_WINDOWS
 
 /**
  * macOS Main Layer
@@ -69,13 +73,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * PUP - Page Up
  * PDN - Page Down
  */
-[_MAC_BASE] = {
-  // 0,      1,       2,       3,     4,     5,     6,      7,     8,     9,       10,      11,           12,           13,        14,      15
-  { KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS,      KC_EQL,       KC_BSPC,   KC_NO,   RGB_MOD   },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC,      KC_RBRC,      KC_BSLASH, KC_NO,   KC_HOME   },
-  { KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,      KC_NO,        KC_ENT,    KC_NO,   KC_PGUP   },
-  { KC_LSFT, KC_NO,   KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,      KC_NO,        KC_RSFT,   KC_UP,   KC_PGDOWN },
-  { KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, KC_NO, KC_NO,   KC_RGUI, MO(_MAC_FN1), MO(_MAC_FN2), KC_LEFT,   KC_DOWN, KC_RGHT   }
+[_BASE] = {
+  // 0,      1,       2,       3,     4,     5,     6,      7,     8,     9,       10,      11,       12,       13,        14,      15
+  { KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,   KC_NO,   RGB_MOD   },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLASH, KC_NO,   KC_HOME   },
+  { KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,  KC_NO,    KC_ENT,    KC_NO,   KC_PGUP   },
+  { KC_LSFT, KC_NO,   KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,    KC_RSFT,   KC_UP,   KC_PGDOWN },
+  { KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, KC_NO, KC_NO,   KC_RGUI, MO(_FN1), MO(_FN2), KC_LEFT,   KC_DOWN, KC_RGHT   }
 },
 
 /**
@@ -133,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  * TODO: Is RGB_TOG okay since it writes to EEPROM?
  */
-[_MAC_FN1] = {
+[_FN1] = {
   // 0,      1,       2,       3,          4,      5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
   { KC_ESC,  KC_F14,  KC_F15,  MAC_EXPOSE, KC_F16, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,  KC_NO,   RGB_TOG },
   { KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,   KC_NO,   KC_NO   },
@@ -165,14 +169,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KPD - Keyboard RGB speed decrease
  * MOD - Swap to Windows mode (persistent)
  */
-[_MAC_FN2] = {
-  // 0,    1,      2,      3,      4,      5,      6,      7,      8,      9,      10,      11,     12,     13,    14,      15
-  { KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11, KC_F12, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22,  KC_F23, KC_F24, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO, RGB_SPI, KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  SET_WIN, KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO }
-},
+[_FN2] = {
+  // 0,    1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,    14,      15
+  { KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,   KC_NO },
+  { KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO, KC_NO,   KC_NO },
+  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,   KC_NO },
+  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPI, KC_NO },
+  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO }
+}
+
+#else
 
 /**
  * Windows Main Layer
@@ -194,13 +200,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * PUP - Page Up
  * PDN - Page Down
  */
-[_WIN_BASE] = {
-  // 0,      1,       2,       3,     4,     5,     6,      7,     8,     9,       10,       11,           12,           13,        14,      15
-  { KC_ESC,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,     KC_MINS,      KC_EQL,       KC_BSPC,   KC_NO,   RGB_MOD   },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,     KC_LBRC,      KC_RBRC,      KC_BSLASH, KC_NO,   KC_HOME   },
-  { KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN,  KC_QUOT,      KC_NO,        KC_ENT,    KC_NO,   KC_PGUP   },
-  { KC_LSFT, KC_NO,   KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,   KC_SLSH,      KC_NO,        KC_RSFT,   KC_UP,   KC_PGDOWN },
-  { KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, KC_NO, KC_NO,   KC_RCTRL, MO(_WIN_FN1), MO(_WIN_FN2), KC_LEFT,   KC_DOWN, KC_RGHT   }
+[_BASE] = {
+  // 0,      1,       2,       3,     4,     5,     6,      7,     8,     9,       10,       11,       12,       13,        14,      15
+  { KC_ESC,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,   KC_NO,   RGB_MOD   },
+  { KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLASH, KC_NO,   KC_HOME   },
+  { KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,    KC_NO,   KC_PGUP   },
+  { KC_LSFT, KC_NO,   KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,   KC_UP,   KC_PGDOWN },
+  { KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, KC_NO, KC_NO,   KC_RCTRL, MO(_FN1), MO(_FN2), KC_LEFT,   KC_DOWN, KC_RGHT   }
 },
 
 /**
@@ -232,8 +238,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KLB - Keyboard RGB light style back
  * KLF - Keyboard RGB light style forward
  */
-[_WIN_FN1] = {
-  // 0,      1,       2,       3,     4,     5,       6,       7,       8,       9,       10,      11,      12,      13,       14,      15
+[_FN1] = {
+  // 0,      1,       2,       3,     4,     5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
   { KC_GRV,  KC_BRID, KC_BRIU, KC_NO, KC_NO, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   RGB_TOG },
   { KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,   KC_NO,   KC_NO   },
   { KC_CAPS, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO   },
@@ -260,14 +266,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KPD - Keyboard RGB speed decrease
  * MOD - Swap to Mac mode (persistent)
  */
-[_WIN_FN2] = {
-  // 0,    1,      2,      3,      4,      5,      6,      7,      8,      9,      10,      11,     12,     13,    14,      15
-  { KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11, KC_F12, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22,  KC_F23, KC_F24, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_NO, RGB_SPI, KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  SET_MAC, KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO }
+[_FN2] = {
+  // 0,    1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,    14,      15
+  { KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,   KC_NO },
+  { KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO, KC_NO,   KC_NO },
+  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,   KC_NO },
+  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPI, KC_NO },
+  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO }
 }
+
+#endif
+
 };
 
 // This seems to always go to index=1, active=1
@@ -319,6 +328,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   mod_state = get_mods();
 
   switch (keycode) {
+#ifndef _K6_WINDOWS
     // FN1 + 3:        Exposé all apps' windows (via Ctrl-Up)
     // FN1 + CTRL + 3: Exposé current apps' windows (via Ctrl-Down)
     // FN1 + CMD + 3:  Show Desktop (via F11)
@@ -356,14 +366,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
       return false;
       break;
-    case SET_MAC:
-      set_single_persistent_default_layer(_MAC_BASE);
-      return false;
-      break;
-    case SET_WIN:
-      set_single_persistent_default_layer(_WIN_BASE);
-      return false;
+#endif
+    default:
+      return true;
       break;
   }
-  return true;
 }
---
+++ keymaps/rules.mk
@@ -0,0 +1,11 @@
+## Custom
+
+# Set Windows keys
+_K6_WINDOWS = no
+
+ifeq ($(strip $(_K6_WINDOWS)), yes)
+	OPT_DEFS += -D_K6_WINDOWS
+endif
+
+# Enable console for debugging
+CONSOLE_ENABLE = yes
