2b2ba78d2a5d584ae1c377bbe97c5efc70cf5614
Joshua Priddle <jpriddle@me.com>
2021-09-08 21:59:25 -0400

    Add LAYOUT() macro

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -73,14 +73,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * PUP - Page Up
  * PDN - Page Down
  */
-[_BASE] = {
-  // 0,      1,       2,       3,     4,     5,     6,      7,     8,     9,       10,      11,       12,       13,        14,      15
-  { KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,   KC_NO,   RGB_MOD   },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLASH, KC_NO,   KC_HOME   },
-  { KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,  KC_NO,    KC_ENT,    KC_NO,   KC_PGUP   },
-  { KC_LSFT, KC_NO,   KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,    KC_RSFT,   KC_UP,   KC_PGDOWN },
-  { KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, KC_NO, KC_NO,   KC_RGUI, MO(_FN1), MO(_FN2), KC_LEFT,   KC_DOWN, KC_RGHT   }
-},
+[_BASE] = LAYOUT(
+  // 0,    1,       2,       3,     4,     5,     6,      7,     8,     9,       10,      11,       12,       13,        14,      15
+  KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,            RGB_MOD,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLASH,          KC_HOME,
+  KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,             KC_PGUP,
+  KC_LSFT,          KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,   KC_UP,   KC_PGDOWN,
+  KC_LCTL, KC_LALT, KC_LGUI,                      KC_SPC,                        KC_RGUI, MO(_FN1), MO(_FN2), KC_LEFT,   KC_DOWN, KC_RGHT
+),
 
 /**
  * macOS FN1 Layer
@@ -137,14 +137,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  * TODO: Is RGB_TOG okay since it writes to EEPROM?
  */
-[_FN1] = {
-  // 0,      1,       2,       3,          4,      5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
-  { KC_ESC,  KC_F14,  KC_F15,  MAC_EXPOSE, KC_F16, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,  KC_NO,   RGB_TOG },
-  { KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,   KC_NO,   KC_NO   },
-  { KC_CAPS, KC_NO,   KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO   },
-  { KC_TRNS, KC_NO,   KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, RGB_SAI, KC_NO   },
-  { KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,      KC_NO,  KC_NO,   RESET,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   RGB_HUD, RGB_SAD, RGB_HUI }
-},
+[_FN1] = LAYOUT(
+  // 0,    1,       2,       3,          4,      5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
+  KC_ESC,  KC_F14,  KC_F15,  MAC_EXPOSE, KC_F16, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,           RGB_TOG,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,            KC_NO,
+  KC_CAPS, KC_NO,   KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,            KC_NO,
+  KC_TRNS,          KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_TRNS, RGB_SAI, KC_NO,
+  KC_TRNS, KC_TRNS, KC_TRNS,                              RESET,                              KC_TRNS, KC_NO,   KC_NO,   RGB_HUD, RGB_SAD, RGB_HUI
+),
 
 /**
  * macOS FN2 Layer
@@ -169,14 +169,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KPD - Keyboard RGB speed decrease
  * MOD - Swap to Windows mode (persistent)
  */
-[_FN2] = {
+[_FN2] = LAYOUT(
   // 0,    1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,    14,      15
-  { KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPI, KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO }
-}
+  KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO,          KC_NO,
+  KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO,          KC_NO,
+  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,          KC_NO,
+  KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO, RGB_SPI, KC_NO,
+  KC_NO, KC_NO,  KC_NO,                          KC_NO,                          KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO
+)
 
 #else
 
@@ -200,14 +200,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * PUP - Page Up
  * PDN - Page Down
  */
-[_BASE] = {
-  // 0,      1,       2,       3,     4,     5,     6,      7,     8,     9,       10,       11,       12,       13,        14,      15
-  { KC_ESC,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,   KC_NO,   RGB_MOD   },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLASH, KC_NO,   KC_HOME   },
-  { KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,    KC_NO,   KC_PGUP   },
-  { KC_LSFT, KC_NO,   KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,   KC_N,  KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,   KC_UP,   KC_PGDOWN },
-  { KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_NO, KC_SPC, KC_NO, KC_NO, KC_NO,   KC_RCTRL, MO(_FN1), MO(_FN2), KC_LEFT,   KC_DOWN, KC_RGHT   }
-},
+[_BASE] = LAYOUT(
+  // 0,    1,       2,       3,    4,    5,    6,      7,    8,    9,       10,       11,       12,       13,        14,      15
+  KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            RGB_MOD,
+  KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLASH,          KC_HOME,
+  KC_ESC,  KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,             KC_PGUP,
+  KC_LSFT,          KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,   KC_UP,   KC_PGDOWN,
+  KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                      KC_RCTRL, MO(_FN1), MO(_FN2), KC_LEFT,   KC_DOWN, KC_RGHT
+),
 
 /**
  * Windows FN1 Layer
@@ -238,14 +238,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KLB - Keyboard RGB light style back
  * KLF - Keyboard RGB light style forward
  */
-[_FN1] = {
-  // 0,      1,       2,       3,     4,     5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
-  { KC_GRV,  KC_BRID, KC_BRIU, KC_NO, KC_NO, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   RGB_TOG },
-  { KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,   KC_NO,   KC_NO   },
-  { KC_CAPS, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO   },
-  { KC_TRNS, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, RGB_SAI, KC_NO   },
-  { KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO,   RESET,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,   RGB_HUD, RGB_SAD, RGB_HUI }
-},
+[_FN1] = LAYOUT(
+  // 0,    1,       2,       3,     4,     5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
+  KC_GRV,  KC_BRID, KC_BRIU, KC_NO, KC_NO, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,            RGB_TOG,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,            KC_NO,
+  KC_CAPS, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,            KC_NO,
+  KC_TRNS,          KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_TRNS, RGB_SAI, KC_NO,
+  KC_TRNS, KC_TRNS, KC_TRNS,                        RESET,                              KC_TRNS, KC_NO,   KC_NO,   RGB_HUD, RGB_SAD, RGB_HUI
+),
 
 /**
  * Windows FN2 Layer
@@ -266,14 +266,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KPD - Keyboard RGB speed decrease
  * MOD - Swap to Mac mode (persistent)
  */
-[_FN2] = {
-  // 0,    1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,    14,      15
-  { KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,   KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPI, KC_NO },
-  { KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO }
-}
+[_FN2] = LAYOUT(
+  // 0,  1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,    14,      15
+  KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO,          KC_NO,
+  KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO,          KC_NO,
+  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO,          KC_NO,
+  KC_NO,         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,          KC_NO, RGB_SPI, KC_NO,
+  KC_NO, KC_NO,  KC_NO,                          KC_NO,                          KC_NO,  KC_NO,  KC_NO,  KC_NO, RGB_SPD, KC_NO
+)
 
 #endif
 
