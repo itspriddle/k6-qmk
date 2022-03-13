91ca2538da13bdc0cb169de6c40fa6868ef4f159
Joshua Priddle <jpriddle@me.com>
2022-03-12 22:05:50 -0500

    Use upstream LAYOUT_65_ansi macro

--- keyboards/keychron/k6/keymaps/ansi-josh/keymap.c
+++ keyboards/keychron/k6/keymaps/ansi-josh/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * PUP - Page Up
  * PDN - Page Down
  */
-[_MAC_BASE] = LAYOUT(
+[_MAC_BASE] = LAYOUT_65_ansi(
   // 0,    1,       2,       3,     4,     5,     6,      7,     8,     9,       10,      11,           12,           13,        14,      15
   KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS,      KC_EQL,       KC_BSPC,            RGB_MOD,
   KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC,      KC_RBRC,      KC_BSLASH,          KC_HOME,
@@ -138,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  * TODO: Is RGB_TOG okay since it writes to EEPROM?
  */
-[_MAC_FN1] = LAYOUT(
+[_MAC_FN1] = LAYOUT_65_ansi(
   // 0,    1,       2,       3,          4,      5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
   KC_ESC,  KC_F14,  KC_F15,  MAC_EXPOSE, KC_F16, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL,           RGB_TOG,
   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,            KC_NO,
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KPI - Keyboard RGB speed increase
  * KPD - Keyboard RGB speed decrease
  */
-[_MAC_FN2] = LAYOUT(
+[_MAC_FN2] = LAYOUT_65_ansi(
   // 0,    1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,    14,      15
   KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO,          KC_NO,
   KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO,          KC_NO,
@@ -198,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * PUP - Page Up
  * PDN - Page Down
  */
-[_WIN_BASE] = LAYOUT(
+[_WIN_BASE] = LAYOUT_65_ansi(
   // 0,    1,       2,       3,    4,    5,    6,      7,    8,    9,       10,       11,           12,           13,        14,      15
   KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,     KC_MINS,      KC_EQL,       KC_BSPC,            RGB_MOD,
   KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,     KC_LBRC,      KC_RBRC,      KC_BSLASH,          KC_HOME,
@@ -236,7 +236,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KLB - Keyboard RGB light style back
  * KLF - Keyboard RGB light style forward
  */
-[_WIN_FN1] = LAYOUT(
+[_WIN_FN1] = LAYOUT_65_ansi(
   // 0,    1,       2,       3,     4,     5,       6,       7,       8,       9,       10,      11,      12,      13,      14,      15
   KC_GRV,  KC_BRID, KC_BRIU, KC_NO, KC_NO, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,            RGB_TOG,
   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_INS,  KC_DEL,  KC_END,  KC_NO,            KC_NO,
@@ -263,7 +263,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * KPI - Keyboard RGB speed increase
  * KPD - Keyboard RGB speed decrease
  */
-[_WIN_FN2] = LAYOUT(
+[_WIN_FN2] = LAYOUT_65_ansi(
   // 0,  1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,    14,      15
   KC_NO, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_NO,          KC_NO,
   KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_NO,          KC_NO,
